75f82b6043180384f1d3f34a947371ae7d353f27
dErnandoV7 <d.ernandov7@gmail.com>
2024-01-18 20:10:41 -0300

    up

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Código referente ao botão de exibir side bar
+const buttonShowSideBarMob = document.querySelector(".show-side-bar-mobile");
+const buttonHideSideBarMob = document.querySelector(".hide-side-bar-mobile");
+const elSideBarMobile = document.querySelector(".side-bar-mobile");
+
+buttonShowSideBarMob.addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  elSideBarMobile.classList.toggle("transitionEnter");
+  buttonShowSideBarMob.classList.toggle("hide");
+});
+
+buttonHideSideBarMob.addEventListener("click", () => {
+  elSideBarMobile.classList.toggle("transitionEnter");
+  buttonShowSideBarMob.classList.toggle("hide");
+});
+
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  if (elSideBarMobile.classList.contains("transitionEnter")) return;
+  if (!el.classList.contains("side-bar-mobile")) buttonHideSideBarMob.click();
+});
